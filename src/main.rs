958cff9b7ffca4aa84d7df350a79e68a9442c3fc
Demon Spawn <alexander.seidel226@gmail.com>
2018-05-13 17:23:51 +0200

    - initial commit for NonogramGenerator

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+/***
+This program shall be capable of reading in an imagefile, create a "contrast map", and from this map,
+create a Nonogram.
+
+1. Read in a picture
+2. Where the contrast from the fields is above a limit, the pixel is set to black
+3. From this map generate a Nonogram (maybe also the game?)
+***/
+
+fn main() {
+    println!("Hello, world!");
+}
